f3ba971cf8126e5d2d11b9ba05b4e93a4694dec4
sunnysaini466 <sunnysaini466@gmail.com>
2023-12-03 13:53:29 +0530

    Chapter 3 - Laying the Foundation

--- App.js
+++ App.js
@@ -1,37 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement(
-  "h1",
-  {
-    id: "sunny",
-    className: "saini",
-    style: {
-      color: "red",
-    },
-  },
-  "Heading 1"
-);
-const heading2 = React.createElement(
-  "h1",
-  {
-    id: "sunny2",
-    className: "saini2",
-    style: {
-      color: "green",
-    },
-  },
-  "Heading 2"
+const Title = (
+  <h2 id="title" key="h1">
+    This is just a React element
+  </h2>
 );
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading1, heading2]
-);
+const HeaderComponent = () => {
+  return (
+    <div>
+      {Title}
+      <h1>This is our functional component</h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+root.render(<HeaderComponent />);
